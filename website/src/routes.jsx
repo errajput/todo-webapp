81d02e1ce6958b5470a4df8565861b40fdefebd4
errajput <dishacsevira@gmail.com>
2024-06-13 10:09:25 +0530

    add comments

--- website/src/routes.jsx
+++ website/src/routes.jsx
@@ -6,6 +6,8 @@ import { Todos } from "./pages/Todos";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 
+// proving the path of layout page, todo page, register page and login page
+// layout page is root page then we go other page which we need to go
 const router = createBrowserRouter([
     {
         element: <Layout />,
