5954716995baa8aa227949aecac8cc7eabc83867
errajput <dishacsevira@gmail.com>
2024-06-13 10:03:55 +0530

    add comments

--- website/src/components/Layout.jsx
+++ website/src/components/Layout.jsx
@@ -1,6 +1,13 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { isLogin, logoutUser } from "../services/http.service";
 
+// create function to navigate login page when user logout the page
+{
+    /* check user already login or not
+if user already login then go home and logout page 
+otherwise go login and register page  */
+}
+
 function Layout() {
     const navigate = useNavigate();
     const logout = () => {
